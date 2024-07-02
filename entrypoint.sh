ea8bf2f5ffb242f0aa13013899f5228fa406e641
Renn F <rennf93@gmail.com>
2024-07-02 02:14:09 +0200

    Updated entrypoint.sh and Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/sh -l
 
-# Debugging: Print all INPUT_ prefixed environment variables
-echo "Debugging INPUT variables:"
-env | grep INPUT_
-
 # Extract inputs from 'with' GitHub context using the INPUT_ prefix
-OWNER="${INPUT_OWNER}"
-REPOSITORY="${INPUT_REPOSITORY}"
-ACCESS_TOKEN="${INPUT_ACCESS_TOKEN}"
-SECRET_NAME="${INPUT_SECRET_NAME}"
-SECRET_VALUE="${INPUT_SECRET_VALUE}"
-
-# Debugging: Print extracted variables
-echo "OWNER: $OWNER"
-echo "REPOSITORY: $REPOSITORY"
-echo "ACCESS_TOKEN: $ACCESS_TOKEN"
-echo "SECRET_NAME: $SECRET_NAME"
-echo "SECRET_VALUE: $SECRET_VALUE"
+export OWNER="${INPUT_OWNER}"
+export REPOSITORY="${INPUT_REPOSITORY}"
+export ACCESS_TOKEN="${INPUT_ACCESS_TOKEN}"
+export SECRET_NAME="${INPUT_SECRET_NAME}"
+export SECRET_VALUE="${INPUT_SECRET_VALUE}"
 
 # Check if required inputs are provided
 if [ -z "$OWNER" ]; then
@@ -45,4 +34,4 @@ if [ -z "$SECRET_VALUE" ]; then
 fi
 
 # Run the Python script with the provided inputs
-python /usr/src/app/run.py "$OWNER" "$REPOSITORY" "$ACCESS_TOKEN" "$SECRET_NAME" "$SECRET_VALUE"
+python /usr/src/app/run.py
